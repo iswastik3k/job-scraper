ecef340e53c7fea4d60afa9797cc4cfd8c89d112
Swastik Majestix <iswastik.3k@gmail.com>
2026-05-28 12:20:02 +0530

    chore: setup PostgreSQL with SQLAlchemy + Alembic, add job_postings table, and init entrypoint

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# src/main.py
+from db.database import engine
+from db.models import Base
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+    print("Database initialized and tables are ready.")
+
+if __name__ == "__main__":
+    init_db()
